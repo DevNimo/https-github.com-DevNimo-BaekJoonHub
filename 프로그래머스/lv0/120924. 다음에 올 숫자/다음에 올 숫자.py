308d663764e75aed677afead6a8e5521c7cf24c2
Lee, SangMin <59852928+DevNimo@users.noreply.github.com>
2023-04-01 14:48:05 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,5 +1,5 @@
 def solution(common):
-    num1, num2, num3 = common[0], common[1], common[2]
+    num1, num2, num3 = common[:3]
     sub1, sub2 = num2-num1, num3-num2
     
     return common[-1] * (sub2//sub1) if sub1 != sub2 else common[-1] + sub1
